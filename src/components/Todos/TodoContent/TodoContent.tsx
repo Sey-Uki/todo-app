d8bb008df9b379067004d1c38fc14486d6ec395f
Sey-Uki <abakarova.diana99@gmail.com>
2022-06-16 01:19:12 +0300

    fetch todos

--- src/components/Todos/TodoContent/TodoContent.tsx
+++ src/components/Todos/TodoContent/TodoContent.tsx
@@ -1,22 +1,46 @@
 import styles from "./TodoContent.module.css";
 import { Button, Divider, List, Checkbox } from "antd";
 import { ReactComponent as DeleteIcon } from "../../../img/delete.svg";
+import { useEffect, useState } from "react";
+import { TODOS_URL } from "../../../utils/constants";
+
+interface ITodo {
+  id: string;
+  todo: string;
+  completed: boolean;
+}
 
 export const TodoContent = () => {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(TODOS_URL);
+      const todosArray = await response.json();
+
+      setTodos(todosArray);
+    })();
+  }, []);
+
   return (
     <div className={styles.todoContent}>
       <div className={styles.todoList}>
         <Divider orientation="left">List</Divider>
         <List bordered>
-          <List.Item className={styles.li_item}>
-            <label>
-              <Checkbox className={styles.check} />
-              todo
-            </label>
-            <button className={styles.delete_img}>
-              <DeleteIcon />
-            </button>
-          </List.Item>
+          {todos.length > 0 &&
+            todos.map((todo: ITodo) => {
+              return (
+                <List.Item className={styles.li_item} key={todo.id}>
+                  <label>
+                    <Checkbox className={styles.check} />
+                    {todo.todo}
+                  </label>
+                  <button className={styles.delete_img}>
+                    <DeleteIcon />
+                  </button>
+                </List.Item>
+              );
+            })}
         </List>
         <div className={styles.btns}>
           <Button className={styles.all_btn} type="primary" ghost>
